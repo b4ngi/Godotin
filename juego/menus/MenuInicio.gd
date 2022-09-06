38d5a2cefb4350c88488a3c34bb6e2cd1606e368
b4ngi <ignaciobenetucci@gmail.com>
2022-09-06 01:06:23 -0300

    Pantalla de carga, carga en segundo plano
    - Pantalla de carga
         - Animacion de godotin
         - Barra de progreso
    - Carga de nivel en segundo plano

--- juego/menus/MenuInicio.gd
+++ juego/menus/MenuInicio.gd
@@ -4,6 +4,7 @@ extends Control
 
 export(String, FILE, "*.tscn") var menu_ajustes = ""
 export(String, FILE, "*.tscn") var nivel_inicial = ""
+export(String, FILE, "*.tscn") var pantalla_carga = ""
 
 
 func _on_BotonSalir_pressed() -> void:
@@ -13,8 +14,9 @@ func _on_BotonOpciones_pressed() -> void:
 	$BotonSFX.play()
 	get_tree().change_scene(menu_ajustes)
 
-func _on_BotonNuevo_pressed():
-	get_tree().change_scene(nivel_inicial)
+func _on_BotonNuevo_pressed() -> void:
+	DatosJuego.nivel_actual = nivel_inicial
+	get_tree().change_scene(pantalla_carga)
 
 func _get_configuration_warning() -> String:
 	if menu_ajustes == "":
